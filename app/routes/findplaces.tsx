6aa2f201492521e38e07ccf3788d814fa59e7132
ZarmDev <darmiyaevdevelop@protonmail.com>
2025-06-27 18:23:30 -0400

    Working location

--- app/routes/findplaces.tsx
+++ app/routes/findplaces.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Circle, Region } from 'react-native-maps';
 import { styles } from '../constants/styles';
 
 import * as Location from 'expo-location';
@@ -8,26 +8,21 @@ import * as Location from 'expo-location';
 const testing = true;
 // Example fake location object for NYC (Central Park)
 const fakeNYCLocation = {
-    coords: {
-        latitude: 40.785091,
-        longitude: -73.968285,
-        altitude: 30,
-        accuracy: 5,
-        altitudeAccuracy: 1,
-        heading: 0,
-        speed: 0,
-    },
-    timestamp: Date.now(),
-};
+    latitude: 40.785091,
+    longitude: -73.968285,
+    latitudeDelta: 1,
+    longitudeDelta: 1,
+}
 
 // Template taken from https://docs.expo.dev/versions/latest/sdk/location/
 export default function FindPlaces() {
-    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    // const [location, setLocation] = useState<Location.LocationObject | null>(null);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
+    const [region, setRegion] = useState<Region>(fakeNYCLocation);
+    const [accuracy, setAccuracy] = useState<number | null>(500);
 
     useEffect(() => {
         async function getCurrentLocation() {
-
             let { status } = await Location.requestForegroundPermissionsAsync();
             if (status !== 'granted') {
                 setErrorMsg('Permission to access location was denied');
@@ -35,10 +30,18 @@ export default function FindPlaces() {
             }
 
             let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
+            if (location) {
+                setRegion({
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                    latitudeDelta: 1,
+                    longitudeDelta: 1,
+                });
+                setAccuracy(location.coords.accuracy)
+            }
         }
         if (testing) {
-            setLocation(fakeNYCLocation as Location.LocationObject);
+            setRegion(fakeNYCLocation);
         } else {
             getCurrentLocation();
         }
@@ -47,31 +50,24 @@ export default function FindPlaces() {
     let text = 'Waiting...';
     if (errorMsg) {
         text = errorMsg;
-    } else if (location) {
-        text = JSON.stringify(location);
+    } else if (region) {
+        text = JSON.stringify(region);
     }
 
     return (
         <View style={styles.container}>
-            {location == null ? <></> : <MapView region={{
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-                latitudeDelta: 0.0922, // for zoom
-                longitudeDelta: 0.0421, // for zoom
-            }} style={styles.map}>
-                <Marker coordinate={{
-                    latitude: location.coords.latitude,
-                    longitude: location.coords.longitude,
-                }}>
-                    <View style={{
-                        height: 20,
-                        width: 20,
-                        borderRadius: 10,
-                        backgroundColor: 'blue',
-                        borderColor: 'white',
-                        borderWidth: 2,
-                    }} />
-                </Marker>
+            {location == null ? <></> : <MapView region={region} style={styles.map}>
+                <Circle
+                    center={{
+                        latitude: region.latitude,
+                        longitude: region.longitude,
+                    }}
+                    // @ts-ignore (will never be null)
+                    radius={accuracy} // in meters
+                    strokeColor="rgba(0,122,255,0.5)"
+                    fillColor="rgba(0,122,255,0.3)"
+                    strokeWidth={2}
+                />
 
             </MapView>}
             {/* <Text>{text}</Text> */}
